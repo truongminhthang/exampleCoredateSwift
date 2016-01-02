156fbc179a4552d5cbe055c2a62b7622621add1f
Trương Thắng <truong.minh.thang@framgia.com>
2016-01-02 08:47:11 +0700

    Initial Commit

---
+++ coredataSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coredataSwift
+//
+//  Created by Trương Thắng on 1/2/16.
+//  Copyright © 2016 Trương Thắng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
